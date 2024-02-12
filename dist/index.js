d7b893c607093a3fd91e07445f5cc63b5b094fc3
Hari Demirev <hari.demirev@gmail.com>
2024-02-12 11:46:02 -0500

    update endpoint

--- dist/index.js
+++ dist/index.js
@@ -31680,7 +31680,7 @@ function uploadSpecAndConfig(specPath, configPath, token, projectName) {
         if (configPath) {
             formData.set('stainlessConfig', yield (0, node_fetch_1.fileFrom)(configPath, 'text/plain'));
         }
-        const response = yield (0, node_fetch_1.default)('https://4816-64-78-254-128.ngrok-free.app/api/spec', {
+        const response = yield (0, node_fetch_1.default)('https://4293-64-78-254-128.ngrok-free.app/api/spec', {
             method: 'POST',
             body: formData,
             headers: {
